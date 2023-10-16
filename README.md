93d0544d275d1336a216034a3dce90c8ac1d9052
oukene <ragcarib@naver.com>
2023-10-16 17:17:47 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# translation_unicode_converter
+ 
---
+++ custom_components/translation_unicode_converter/sensor.py
@@ -0,0 +1,42 @@
+"""Support for switches which integrates with other components."""
+from __future__ import annotations
+
+from typing import Any
+import logging
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+import os
+import json
+
+_LOGGER = logging.getLogger(__name__)
+
+async def async_setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    async_add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
+    """Set up the template switches."""
+
+    _LOGGER.debug("call unicode converter setup2")
+    _LOGGER.debug("config" + str(config.get("components")))
+
+    for component in config.get("components"):
+        filePath = "custom_components/" + component.get("name") + "/translations/ko.json"
+        _LOGGER.debug("filePath : " + str(filePath))
+        data = None
+        if os.path.exists(filePath):
+            with open(filePath, "r") as f:
+                _LOGGER.debug("load json")
+                data = json.load(f)
+            if data != None:
+                with open(filePath, "w") as f:
+                    json.dump(data, f, sort_keys=component.get("sort", False), indent=4)
+
+
+    
+    
+    _LOGGER.debug("real path : " + str(os.listdir("custom_components")))
+
+
